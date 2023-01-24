52c90b696a7d3829568dc7928ebe7dcbc40e42ba
msilis <mikssilis@gmail.com>
2023-01-24 10:50:50 +0000

    clear inputs on submitting interest calculation

--- src/Components/Pages/interest.js
+++ src/Components/Pages/interest.js
@@ -12,6 +12,7 @@ function Interest(){
     //Get values from inputs
     const getAmount = (e) => {
         setShopAmount(e.target.value)
+        
     }
 
     const getMonths = (e) => {
@@ -19,19 +20,24 @@ function Interest(){
     }
     //Do calculation
     function calculatePayment(){
+
         return (Math.round(((Number(shopAmount) + Number(shopAmount * .2) )/Number(monthAmount))*100)/100).toFixed(2)
     }
     //Show result of calculation in DOM
     function showResult(){
-        setResultText(<p>Borrowing £{shopAmount} for {monthAmount} months will give you a monthly payment of £{calculatePayment()} for a total amount paid of £{Number(shopAmount) + Number(shopAmount *.2)}.</p>)
+        setResultText(<p>Borrowing £{shopAmount} for {monthAmount} months at 20% interest will give you a monthly payment of £{calculatePayment()} for a total amount paid of £{Number(shopAmount) + Number(shopAmount *.2)}.</p>)
+        setShopAmount('');
+        setMonthAmount('');
     }
    
     return(
         <div className="interest-calc-container">
             <h2>Interest calculator</h2>
             <div className="input-container">
-            <input placeholder="Purchase amount" onChange={getAmount}></input>
-            <input placeholder="How many months?" onChange={getMonths}></input>
+            <label>Amount to borrow</label>    
+            <input value={shopAmount} onChange={getAmount}></input>
+            <label>How many months?</label>
+            <input value={monthAmount} onChange={getMonths}></input>
             <Button onClick={showResult}>Calculate</Button>
             </div>
             <div className="interest-result">
